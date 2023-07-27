c761001542266bc4466f5e26ec2ff2d6294e6925
Jake Samuels <samuels242@googlemail.com>
2023-07-27 23:00:22 +0100

    Resize QR code

--- components/QRCode.jsx
+++ components/QRCode.jsx
@@ -4,11 +4,11 @@ const QRCode = ({ setShowQr, profileUrl }) => {
   // Get the current page URL
 
   return (
-    <div className="fixed w-full h-screen  top-0 right-0 left-0 bottom-0 bg-black opacity-90 flex flex-col items-center justify-center">
-      <div className="border mb-8">
-        <QRCodeReact value={profileUrl} />
+    <div className="fixed w-full h-screen top-0 right-0 left-0 bottom-0 bg-black opacity-90 flex flex-col items-center justify-center">
+      <div className="border-4 mb-10 rounded-sm">
+        <QRCodeReact value={profileUrl} bgColor="white" size={175} />
       </div>
-      <button className="blue_btn" onClick={() => setShowQr(false)}>
+      <button className="blue_btn opacity-100" onClick={() => setShowQr(false)}>
         Close
       </button>
     </div>
