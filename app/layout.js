f2646afd7d4639e3e3cb8429d931a3ac42e136cb
Jake Samuels <samuels242@googlemail.com>
2023-07-27 18:44:23 +0100

    Add favicon

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
-  title: "BizLinks",
+  title: "BizLink",
   description:
     "Easily store and share your business profiles and review site links",
 };
@@ -14,6 +14,9 @@ import React from "react";
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/assets/logo.png" sizes="any" />
+      </head>
       <body>
         <Provider>
           <main>
